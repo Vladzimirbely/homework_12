5dc2baebbc4e8a6829a448119b97bf039652ae2c
Uladzimir Bely <vladzimir.bely@gmail.com>
2024-04-15 15:51:13 +0300

    add env, secure login and password

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
+import os
 import pytest
 from selene import browser
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from utils import attach
+from dotenv import load_dotenv
 
 DEFAULT_BROWSER_VERSION = "100.0"
 
@@ -11,6 +13,10 @@ def pytest_addoption(parser):
         '--browser_version',
         default='100.0'
     )
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
 
 @pytest.fixture(scope='function', autouse=True)
 def setup_browser(request):
@@ -26,8 +32,12 @@ def setup_browser(request):
         }
     }
     options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
